130df4baea38f55e3aa1d933fc944fcd8cd6e235
Sergei US <ustimenko.sv@gmail.com>
2022-09-21 23:11:00 +0300

    Initial commit

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -3,25 +3,18 @@ package ru.netology.carddelivery;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.chrome.ChromeOptions;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.*;
-
 
 public class CardDeliveryTest {
 
     @BeforeEach
     void setUp() {
-//        ChromeOptions options = new ChromeOptions();
-//        options.addArguments("--disable-dev-shm-usage");
-//        options.addArguments("--no-sandbox");
-//        options.addArguments("--headless");
         open("http://localhost:9999/");
     }
 
@@ -36,6 +29,32 @@ public class CardDeliveryTest {
         $("[class=\"button__text\"]").click();
         $("#root > div > div > div.notification__content").shouldBe(visible, Duration.ofSeconds(15));
         $("#root > div > div > div.notification__title").shouldHave(text("Успешно!"));
-        $("#root > div > div > div.notification__content").shouldHave(text("Встреча успешно забронирована на "+ date));
+        $("#root > div > div > div.notification__content").shouldHave(text("Встреча успешно забронирована на " + date));
+    }
+
+    @Test
+    void shouldTestCityListOpen() {
+        $x("//*[@placeholder=\"Город\"]").setValue("Мо");
+        $x("/html/body/div[3]/div/div/div").shouldBe(visible);
+    }
+
+    @Test
+    void shouldTestCalendarDate() {
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/span/button").click();
+        $x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[6]/td[4]").click();
+    }
+
+    @Test
+    void shouldTestCalendarDateInNextMonth() {
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/span/button").click();
+        $x("/html/body/div[2]/div/div/div/div/div/div/div[4]").click();
+        $x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[4]/td[5]").click();
+    }
+
+    @Test
+    void shouldTestCalendarDateInAYear() {
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/span/button").click();
+        $x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[1]/th[7]").click();
+        $x("/html/body/div[2]/div/div/div/div/div/table/tbody/tr[4]/td[5]").click();
     }
 }
