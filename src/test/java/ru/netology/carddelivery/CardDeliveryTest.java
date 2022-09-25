4e924fa22d0baa85816350d0e0fd63c7c8bc257d
Sergei US <ustimenko.sv@gmail.com>
2022-09-25 15:03:01 +0300

    Initial commit

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -12,8 +12,7 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.exactText;
-import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -53,7 +52,7 @@ public class CardDeliveryTest {
         $(".input__popup>div>div>div>div>div:nth-child(3)").click();
         $("[class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").click();
         $("[class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").shouldBe(visible, Duration.ofSeconds(10));
-        $("[class='icon icon_size_m icon_name_calendar icon_theme_alfa-on-white']").find("[data-day='1664485200000']");
+        $("[data-day='1664485200000']").shouldBe(interactable);
         $("[data-day='1664485200000']").click();
         String meetingDate = $("[placeholder='Дата встречи']").getValue();
         $("[name='name']").setValue("Смит Джон");
