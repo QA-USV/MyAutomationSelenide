c3e4edce496e77d25cc25b963b6acd09aa47fff2
Sergei US <ustimenko.sv@gmail.com>
2022-09-25 16:38:01 +0300

    Initial commit

--- src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
+++ src/test/java/ru/netology/carddelivery/CardDeliveryTest.java
@@ -1,12 +1,13 @@
 package ru.netology.carddelivery;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.commands.ShouldBe;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.Select;
+
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
@@ -60,6 +61,7 @@ public class CardDeliveryTest {
         $(".notification__title").shouldHave(exactText("Успешно!"));
         $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + meetingDate));
     }
+
 
     @Test
     void shouldTestCalendarDateNextMonth() {
